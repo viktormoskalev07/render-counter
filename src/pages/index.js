12131a78de49d20e6a913300622419977926786d
Shangri <shangrialena@gmail.com>
2024-05-24 13:32:09 +0200

    fixed path

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import { Inter } from "next/font/google";
 import {SwrParent} from "@/screens/swr/parent";
 import ContextVsRedux from "@/pages/context-vs-redux";
 import State from "@/pages/state";
-import OwnChildrenState from "@/screens/state-dependencies/own-children-state/own-children-state";
 import DependentState from "@/pages/dependent-state";
+import OwnChildrenState from "@/pages/own-children-state";
 
 const inter = Inter({ subsets: ["latin"] });
 
